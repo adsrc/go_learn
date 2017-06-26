0ee9fe8b9b6c2131283c58868bf9b5654990cf05
Aditya Chetan <achetan40@gmail.com>
2017-06-26 18:54:29 +0530

    Variables and Statements

---
+++ var_statements.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	fmt.Println("go"+"lang")
+
+	var a int = 9
+	b := "Aditya"			// Short-hand for declaring variables
+	fmt.Println(a,b)
+
+	fmt.Println(true||false)
+	
+	fmt.Println(true&&false)
+
+
+}
